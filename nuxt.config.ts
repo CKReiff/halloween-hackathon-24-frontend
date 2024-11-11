2c5e84e7073d5df2c9cbd1e978963469d8fc6fe9
Martin Høite <mht@novicell.dk>
2024-11-11 19:29:54 +0100

    Initial setup for integrating with Enterspeed & install packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      enterspeedApiKey: process.env.NUXT_PUBLIC_ENTERSPEED_API_KEY,
+      enterspeedApiBase: process.env.NUXT_PUBLIC_ENTERSPEED_API_BASE
+    }
+  },
+
   routeRules: {
-    // prerender index route by default
     '/': { prerender: true },
   },
+
+  compatibilityDate: '2024-11-11',
 });
